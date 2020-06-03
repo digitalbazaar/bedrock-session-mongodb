57f7164f4c14fab730556224fa256941b0e3af39
Andrew L Jones <ajones@digitalbazaar.com>
2020-06-03 10:42:45 -0400

    Add test to ensure session store is created.

--- test/mocha/10-api.js
+++ test/mocha/10-api.js
@@ -2,7 +2,21 @@
  * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
  */
 'use strict';
+const bedrock = require('bedrock');
+const {config} = bedrock;
 
-describe('session-mongodb API', () => {
-  it('should work');
+describe('session-mongodb API', function() {
+  let store;
+  before(function() {
+    store = config.express.session.store;
+  });
+  it('should create a session store', async function() {
+    should.exist(store);
+    store.should.be.an('object');
+    should.exist(store.state);
+    store.state.should.be.a('string');
+    store.state.should.equal('connected');
+    should.exist(store.db);
+    should.exist(store.client);
+  });
 }); // end session-mongodb API
